47f8615e48ef2797061ec497a28d5206fc15cb15
Alican Yüksel <alicanyuksel@outlook.com>
2021-04-20 12:24:50 +0200

    Add a function to connect to API

--- actions/actions.py
+++ actions/actions.py
@@ -3,6 +3,48 @@ from typing import Any, Text, Dict, List
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.forms import FormAction
+from dotenv import load_dotenv
+
+import os
+import json
+import requests
+
+load_dotenv()
+
+# info for api / airtable.com
+airtable_api_key=os.getenv("AIRTABLE_API_KEY")
+base_id=os.getenv("BASE_ID")
+table_name=os.getenv("TABLE_NAME")
+
+
+def create_health_log(confirm_exercise, exercise, sleep, diet, stress, goal):
+    request_url = f"https://api.airtable.com/v0/{base_id}/{table_name}"
+
+    headers = {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+        "Authorization": f"Bearer {airtable_api_key}"
+    }
+
+    data = {
+        "fields": {
+            "Exercised?": confirm_exercise,
+            "Type of exercise": exercise,
+            "Amount of sleep": sleep,
+            "Stress": stress,
+            "Diet": diet,
+            "Goal": goal
+        }
+    }
+
+    try:
+        response = requests.post(request_url, headers=headers, data=json.dumps(data))
+        response.raise_for_status()
+    except requests.exceptions.HTTPError as err:
+        raise SystemExit(err)
+    
+    return response
+    print(response.status_code)
 
 
 class HealthForm(FormAction):
@@ -23,6 +65,16 @@ class HealthForm(FormAction):
         tracker: Tracker,
         domain: Dict[Text, Any]
     ) -> List[Dict]:
+        response = create_health_log(
+            tracker.get_slot("confirm_exercise"),
+            tracker.get_slot("exercise"),
+            tracker.get_slot("sleep"),
+            tracker.get_slot("diet"),
+            tracker.get_slot("stress"),
+            tracker.get_slot("goal")
+        )
+
+        dispatcher.utter_message("Thanks, your answers have been recorded!")
         return []
 
     def slot_mappings(self):
